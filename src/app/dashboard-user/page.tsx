b2bd94b44a0b0af62d219e6485625eb8d9574ee5
EngJao89 <eng_joao@outlook.com>
2025-05-12 23:58:46 -0300

    feat(gbw17-title-dashboard): create title for dashboard user

--- src/app/dashboard-user/page.tsx
+++ src/app/dashboard-user/page.tsx
@@ -4,6 +4,9 @@ export default function DashboardUser() {
   return(
     <div>
       <Header />
+      <div className="mt-6">
+        <h1 className="text-zinc-50 text-sm font-bold">Dashboard</h1>
+      </div>
     </div>
   )
 }
