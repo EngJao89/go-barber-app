783487038e30f097b771d2817da34a17669ff520
EngJao89 <eng_joao@outlook.com>
2025-05-19 23:32:51 -0300

    feat(gbw22-form-barber-logged): create function for login barber space

--- src/app/login-barber/page.tsx
+++ src/app/login-barber/page.tsx
@@ -2,16 +2,75 @@
 
 import Image from "next/image"
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { FiArrowLeft, FiLogIn } from "react-icons/fi";
+import { z } from "zod";
+import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "react-toastify";
+import axios from 'axios';
 
 import logo from '../../../public/logo-v1.png';
 import homeImg from '../../../public/home-gb.png';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import api from "@/lib/axios";
+import { FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 
+const loginSchema = z.object({
+  email: z.string().email("E-mail é obrigatório"),
+  password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
+});
+
+export type LoginSchema = z.infer<typeof loginSchema>;
 
 export default function LoginBarber(){
   const router = useRouter();
+  const methods = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const { handleSubmit, formState: { errors } } = methods;
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem('authToken');
+      if (token) {
+        router.replace('/dashboard-barber');
+      }
+    }
+  }, [router]);
+
+    const onSubmit = async (data: LoginSchema) => {
+    try {
+      if (!data.email || !data.password) {
+        toast.warning('Por favor, forneça o nome de usuário e a senha', {theme: "light"});
+        throw new Error('Por favor, forneça o nome de usuário e a senha');
+      }
+
+      const response = await api.post('auth-barber/login', data, { withCredentials: true });
+
+      if (response.data.accessToken) {
+        localStorage.setItem('authUserToken', response.data.accessToken);
+        toast.success(`Usuário Logado: ${data.email}, Seja Bem vindo!`, {theme: "light"})
+        router.replace('/dashboard-barber');
+      } else {
+        toast.error('Token não encontrado na resposta', {theme: "light"})
+        throw new Error('Token não encontrado na resposta');
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = 'Login falhou. Por favor, verifique suas credenciais e tente novamente.';
+        toast.error(axiosError, {theme: "light"});
+      } else {
+        toast.error('Ocorreu um erro inesperado. Tente novamente mais tarde.', {theme: "light"});
+      }
+    }
+  };
 
   return(
     <div className="flex min-h-screen">
@@ -27,20 +86,53 @@ export default function LoginBarber(){
           </h1>
 
           <div className="w-full space-y-4">
-            <Input 
-              className="w-full h-12 px-4 text-zinc-100 bg-transparent border-zinc-400 rounded-lg focus:border-orange-500" 
-              placeholder="Email"
-            />
+            <FormProvider {...methods}>
+              <form onSubmit={handleSubmit(onSubmit)} className="relative z-10">
+                <FormField
+                  name="email"
+                  control={methods.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input 
+                          placeholder="Seu Usuário" 
+                          {...field} 
+                          value={field.value || ''} 
+                          className="w-full h-12 px-4 text-zinc-100 bg-transparent border-zinc-400 rounded-lg focus:border-orange-500" 
+                        />
+                      </FormControl>
+                      {errors.email && <FormMessage className="ml-4 text-zinc-400">{errors.email.message}</FormMessage>}
+                    </FormItem>
+                  )}
+                />
 
-            <Input 
-              className="w-full h-12 px-4 text-zinc-100 bg-transparent border-zinc-400 rounded-lg focus:border-orange-500" 
-              placeholder="Senha"
-              type="password"
-            />
+                <FormField
+                  name="password"
+                  control={methods.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input 
+                          type="password" 
+                          placeholder="Sua Senha" 
+                          {...field} 
+                          value={field.value || ''} 
+                          className="w-full h-12 px-4 text-zinc-100 bg-transparent mt-4 border-zinc-400 rounded-lg focus:border-orange-500" 
+                        />
+                      </FormControl>
+                      {errors.password && <FormMessage className="ml-4 text-zinc-400">{errors.password.message}</FormMessage>}
+                    </FormItem>
+                  )}
+                />
 
-            <Button className="w-full h-12 bg-orange-600 hover:bg-orange-700 rounded-lg">
-              <span className="text-zinc-100 font-medium hover:text-zinc-600">Entrar</span>
-            </Button>
+                <Button 
+                  type="submit"
+                  className="w-full h-12 mt-4 bg-orange-600 hover:bg-orange-800 rounded-lg"
+                >
+                  <span className="text-zinc-100 font-medium hover:text-zinc-600">Entrar</span>
+                </Button>
+              </form>
+            </FormProvider>
 
             <Button className="w-full flex justify-center items-center">
               <h1 className="text-zinc-100 font-bold hover:text-zinc-600">Esqueci minha senha</h1>
