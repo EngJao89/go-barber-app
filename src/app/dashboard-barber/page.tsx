1a767debe1e743ba346638f9d65560ed9ec1bd4b
EngJao89 <eng_joao@outlook.com>
2025-07-22 23:04:27 -0300

    feat(gbw41-labe-date): create label of date

--- src/app/dashboard-barber/page.tsx
+++ src/app/dashboard-barber/page.tsx
@@ -44,6 +44,10 @@ export default function DashboardBarber() {
           <h1 className="text-zinc-400 text-xl font-bold">Agenda do dia</h1>
           <p className="text-orange-500 text-base font-normal">Hoje | Dia 15 | Terça feira</p>
         </div>
+                <div className="ml-2 mr-2 mt-14">
+          <h1 className="text-zinc-400 text-xl font-bold">Agenda do dia</h1>
+          <p className="text-orange-500 text-base font-normal">Hoje | Dia 22 | Terça feira</p>
+        </div>
       </div>
     </div>
   )
