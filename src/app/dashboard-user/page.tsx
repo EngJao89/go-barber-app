25e6b7f50cbc5776938e89f18c9221bc28260244
EngJao89 <eng_joao@outlook.com>
2025-07-29 23:16:24 -0300

    feat(gbw45-label-date-baber): create label date barber scheduling

--- src/app/dashboard-user/page.tsx
+++ src/app/dashboard-user/page.tsx
@@ -29,6 +29,10 @@ export default function DashboardUser() {
           <h1 className="text-zinc-400 text-xl font-bold">Meu Agendamento</h1>
           <p className="text-orange-500 text-base font-normal">Hoje | Dia 02 | Quarta feira</p>
         </div>
+        <div className="ml-2 mr-2 mt-14">
+          <h1 className="text-zinc-400 text-xl font-bold">Meu Agendamento</h1>
+          <p className="text-orange-500 text-base font-normal">Hoje | Dia 29 | Terça feira</p>
+        </div>
       </div>
     </div>
   )
