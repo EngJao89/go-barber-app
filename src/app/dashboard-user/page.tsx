69cd0d8f5b86a21976818fb35c20a687a0f21be5
EngJao89 <eng_joao@outlook.com>
2025-08-16 20:58:26 -0300

    feat(gbw58-new-card-scheduling): create new card for schedulings

--- src/app/dashboard-user/page.tsx
+++ src/app/dashboard-user/page.tsx
@@ -1,4 +1,5 @@
 import { HeaderUser } from "@/components/HeaderUser";
+import { CardScheduling } from "@/components/CardScheduling";
 import { getCurrentDate } from "@/utils/getTimeStamp";
 
 export default function DashboardUser() {
@@ -12,34 +13,10 @@ export default function DashboardUser() {
           <h1 className="text-zinc-50 text-4xl font-bold">Meus horários</h1>
           <p className="text-orange-500 text-base font-normal">Hoje | Dia {currentDate.day} | {currentDate.weekday}</p>
         </div>
-
-        <div className="ml-2 mr-2 mt-14">
-          <h1 className="text-zinc-400 text-xl font-bold">Meu Agendamento</h1>
-          <p className="text-orange-500 text-base font-normal">Hoje | Dia 04 | Quarta feira</p>
-        </div>
-
-        <div className="ml-2 mr-2 mt-14">
-          <h1 className="text-zinc-400 text-xl font-bold">Meu Agendamento</h1>
-          <p className="text-orange-500 text-base font-normal">Hoje | Dia 04 | Quarta feira</p>
-        </div>
-
-        <div className="ml-2 mr-2 mt-14">
-          <h1 className="text-zinc-400 text-xl font-bold">Meu Agendamento</h1>
-          <p className="text-orange-500 text-base font-normal">Hoje | Dia 23 | Segunda feira</p>
-        </div>
-
-        <div className="ml-2 mr-2 mt-14">
-          <h1 className="text-zinc-400 text-xl font-bold">Meu Agendamento</h1>
-          <p className="text-orange-500 text-base font-normal">Hoje | Dia 02 | Quarta feira</p>
-        </div>
-        <div className="ml-2 mr-2 mt-14">
-          <h1 className="text-zinc-400 text-xl font-bold">Meu Agendamento</h1>
-          <p className="text-orange-500 text-base font-normal">Hoje | Dia 29 | Terça feira</p>
-        </div>
-        <div className="ml-2 mr-2 mt-14">
-          <h1 className="text-zinc-400 text-xl font-bold">Meu Agendamento</h1>
-          <p className="text-orange-500 text-base font-normal">Hoje | Dia 12 | Terça feira</p>
-        </div>
+      </div>
+      <div className="m-6">
+        <h1 className="text-zinc-300 text-xl font-bold">Atendimento Próximo</h1>
+        <CardScheduling />
       </div>
     </div>
   )
