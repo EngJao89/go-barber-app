355a04a22f71a1e884874a0bace98dd545fff57f
EngJao89 <eng_joao@outlook.com>
2025-09-04 23:44:53 -0300

    feat(gbw76-header-pages): create header for users and barbers pages

--- src/app/profile-barber/page.tsx
+++ src/app/profile-barber/page.tsx
@@ -1,6 +1,9 @@
+import { HeaderBarber } from "@/components/HeaderBarber"
+
 export default function ProfileBarber() {
   return(
     <div>
+      <HeaderBarber />
       <h1>Profile Barber</h1>
     </div>
   )
