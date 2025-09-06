42d85f98e5e8b3eace6134bcea227eb771f6c23f
EngJao89 <eng_joao@outlook.com>
2025-09-05 23:14:53 -0300

    refactor(gbw79-title-page-barber): create title for page profile barber

--- src/app/profile-barber/page.tsx
+++ src/app/profile-barber/page.tsx
@@ -4,7 +4,9 @@ export default function ProfileBarber() {
   return(
     <div>
       <HeaderBarber />
-      <h1>Profile Barber</h1>
+      <div className="flex justify-center items-center m-6">
+        <h1 className="text-zinc-50 text-4xl font-bold">Profile Barber</h1>
+      </div>
     </div>
   )
 }
